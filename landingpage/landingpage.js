adc8022bc05caf7d7e03221ded5b43a1e6cadafc
Tejas <1rn21cs171.Tejas@gmail.com>
2023-08-22 21:48:29 +0530

    made css better

---
+++ landingpage/landingpage.js
@@ -0,0 +1,18 @@
+const spans = document.getElementsByTagName("span");
+const login = spans[0];
+const signup = spans[1];
+
+const main_landing = document.querySelector('.mainlanding');
+const signup_div = document.querySelector('.sign-up-div');
+
+login.addEventListener("click",()=>{
+    signup_div.style.display = 'block';
+    main_landing.style.display = 'none';
+});
+
+signup.addEventListener("click",()=>{
+    signup_div.style.display = 'none';
+    main_landing.style.display = 'flex';
+});
+
+
